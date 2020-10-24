4867a84a8f617e5e8006f7012ec79d9a784725cd
happlyin <54892429+happlyin@users.noreply.github.com>
2020-10-24 15:50:04 +0900

    [SLOVE] Q177

---
+++ OCJAP/src/Q177/Q177.java
@@ -0,0 +1,26 @@
+package Q177;
+
+public class Q177 {
+	public static void main(String[] args) {
+	}
+}
+/* 문제
+Which statement is true about the main() method?
+A. It is invoked by JRE
+B. It is a final method
+C. It returns true if it is executed successfully at run time
+D. It must be defined within a public class
+ */
+
+/* 한국어 변역
+main()에 대한 설명으로 옳은 것은?
+A. 그것은 JRE에 의해 작동된다.
+B. 그것은 최종적인 메서드이다.
+C. 런타임에 성공적으로 실행되면 true를 반환함
+D. 공개 계층 내에서 정의되어야 함
+ */
+
+//답은 A이다.
+// B가 답이 아닌 이유는 가장먼저 실행되는 최초 메서드이기 때문이다.
+// C가 답이 아닌 이유는 런타임에 성공적으로 실행 되면 true를 반환하는 것은 조거문이나 다른 등등이다. main()은 포함되지 않는다.
+// D가 답이 아닌 이유는 어디서 정의 되는지 상관이없다. 단 클래스 내에서만 정의 되면 된다.
