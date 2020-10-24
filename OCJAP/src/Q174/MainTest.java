63d5a47e1ee9441093e15a980a382550a486d855
happlyin <54892429+happlyin@users.noreply.github.com>
2020-10-24 14:51:56 +0900

    [SLOVE] Q174

---
+++ OCJAP/src/Q174/MainTest.java
@@ -0,0 +1,11 @@
+package Q174;
+
+public class MainTest {
+	public static void main(String[] args) {
+		System.out.println("String main " + args[0]);
+	}
+}
+// 실행코드 : java MainTest "1 2 3"
+// 결과 : String main 1 2 3
+// 정답 : C
+// 이유 : ,로 구분한다.
