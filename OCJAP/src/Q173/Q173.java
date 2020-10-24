a30ca3214447c18689d7a42d34a2265dff73b60b
happlyin <54892429+happlyin@users.noreply.github.com>
2020-10-24 13:05:21 +0900

    [SLOVE] Q173

---
+++ OCJAP/src/Q173/Q173.java
@@ -0,0 +1,10 @@
+package Q173;
+
+public class Q173 {
+	public static void main(String[] args) {
+		String[] planets = {"Mercury", "Venus", "Earth", "Mars"};
+		
+		System.out.println(planets.length); //length는 요소의 갯수를 리턴한다.
+		System.out.println(planets[1].length()); //length()는 요소의 길이를 리턴한다.
+	}
+}
