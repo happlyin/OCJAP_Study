33e6a16f3f5bc5095ca1a147f85b48f64e0cc59c
happlyin <54892429+happlyin@users.noreply.github.com>
2020-10-24 11:59:12 +0900

    [SLOVE] Q171

---
+++ OCJAP/src/Q171/q171.java
@@ -0,0 +1,19 @@
+package Q171;
+
+public class q171 {
+	public static void main(String[] args) {
+		int[] arry = {1, 2, 3, 4, 5};
+	}
+}
+/*
+1. Process all the elements of the array in the order of entry.
+2. Process all the elements of the array in the reverse order of entry.
+3. Process alternating elements of the array in the order of entry.
+
+1. 배열의 모든 요소를 항목순으로 처리한다.
+2. 배열의 모든 요소를 입력의 역순으로 처리한다.
+3. 배열의 교대 원소를 입력순으로 처리한다.
+
+1번과 2번은 for와 for each를 사용하여 처리가 가능하지만 3번은 불가능 하다.
+그래서 답은 D, E
+ */
